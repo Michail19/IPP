54f05e9019341be7f73cedfbd9085d4cc74ee95a
Michail19 <Mi.ershov.2017@yandex.ru>
2025-09-08 09:32:31 +0300

    upd

--- src/main/java/practice1/Main.java
+++ src/main/java/practice1/Main.java
@@ -42,7 +42,6 @@ public class Main {
         GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
                 HTTP_TRANSPORT, JSON_FACTORY, GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in)), SCOPES)
                 .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
-                .setAccessType("offline")
                 .build();
         LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
         return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
