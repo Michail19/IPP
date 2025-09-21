207c28e051bdacbe24b41b2780b769e76cfcf5ab
Michail19 <Mi.ershov.2017@yandex.ru>
2025-09-21 09:57:53 +0300

    Structure

--- Task5/phonebook-app/app.module.ts
+++ Task5/phonebook-app/app.module.ts
@@ -1,26 +1,3 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { AppComponent } from './app.component';
-import { ContactDetailsComponent } from './contacts/contact-details/contact-details.component';
-import { ContactListComponent } from './contacts/contactlist/contact-list.component';
-@NgModule({
-    declarations: [
-        AppComponent,
-        ContactDetailsComponent,
-        ContactListComponent
-    ],
-    imports: [
-        BrowserModule,
-        FormsModule,
-        HttpModule
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
-})
-export class AppModule { }
-
 var express = require("express");
 var bodyParser = require("body-parser");
 var mongodb = require("mongodb");
@@ -31,4 +8,3 @@ app.use(bodyParser.json());
 // Создание ссылки на каталог сборки Angular
 var distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
-
---
+++ Task5/phonebook-app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AppComponent } from './app.component';
+import { ContactDetailsComponent } from './contact-details/contact-details.component';
+import { ContactListComponent } from './contactlist/contact-list.component';
+@NgModule({
+    declarations: [
+        AppComponent,
+        ContactDetailsComponent,
+        ContactListComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
